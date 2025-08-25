6404c4df668c86da56f9ea257af9e455966ace15
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2025-08-25 23:24:06 +0530

    Fix script

--- .github/scripts/pr-builder.sh
+++ .github/scripts/pr-builder.sh
@@ -83,7 +83,10 @@ if [ "$REPO" = "product-is" ]; then
   echo "PR is for the product-is itself. Start building with test..."
   echo "=========================================================="
   cd product-is-$BUILDER_NUMBER
+
+  git stash
   git checkout next
+  git stash pop
 
   echo ""
   echo "Applying PR $PULL_NUMBER as a diff..."
